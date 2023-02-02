be25bff38b2f8f3ff85296c15a56456eff087eda
kin3303 <48729027+kin3303@users.noreply.github.com>
2023-02-02 11:05:51 +0900

    Create template.mk

---
+++ template.mk
@@ -0,0 +1,17 @@
+.PHONY: $(COMMON_TARGETS)
+
+all: build push pull
+
+build:
+	docker build -t kin3303/$(MODULE_NAME):$(TAG).
+
+push: build
+	docker push kin3303/$(MODULE_NAME):$(TAG)
+
+pull: 
+	docker pull kin3303/$(MODULE_NAME):$(TAG)
+
+clean: cleanimages
+
+cleanimages:
+	docker rmi kin3303/$(MODULE_NAME):$(TAG)
